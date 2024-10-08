dfa42c22084c8776ab21efa316631ba1e3fd99ed
Simplysaad <148543136+Simplysaad@users.noreply.github.com>
2024-10-08 16:26:53 +0100

    Update db.js

--- server/Config/db.js
+++ server/Config/db.js
@@ -1,1 +1,23 @@
 //db.js
+
+
+const mongoose = require('mongoose')
+
+const connectDb = async() =>{
+
+
+  try{    
+   mongoose.set('strictQuery', false)
+    
+    let conn = await mongoose.connect(process.env.MONGO_URI)
+
+    console.log(`MongoDB connected to ${conn.connection.host}`)
+
+    
+  }
+  catch(err){
+    console.log(err)
+  }
+}
+
+module.exports = connectDb
