2b250b8f5e5ef21a1de9b59a0730f8b518a19362
Simplysaad <148543136+Simplysaad@users.noreply.github.com>
2024-10-17 14:13:48 +0100

    Added error handling to db.js

--- server/Config/db.js
+++ server/Config/db.js
@@ -11,12 +11,13 @@ const connectDb = async() =>{
     
     let conn = await mongoose.connect(process.env.MONGO_URI)
 
+    if(!conn) throw new Error(`trouble while connecting to ${conn.connection.host}`)
     console.log(`MongoDB connected to ${conn.connection.host}`)
 
     
   }
   catch(err){
-    console.log(err)
+    console.error(err)
   }
 }
 
