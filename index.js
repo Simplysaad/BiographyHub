c2c76b74ebb462691e6415f02ab187587c09b4db
Simplysaad <148543136+Simplysaad@users.noreply.github.com>
2024-10-17 14:10:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-//index.js
-//import {POSTS, findPostById} from './postDb.js'
 
-require('dotenv').config()
+
 const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-//const bodyParser = require('body-parser')
-const connectDb = require('./server/config/db')
-
+const session = require('express-session')
+const connectDb = require('./server/config/db.js')
+const expressLayouts = require("express-ejs-layouts")
+require('dotenv').config()
 
 const app = express()
-let PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT
 app.listen(PORT, ()=>{
   console.log(`app listening at http://localhost:${PORT}`)
 })
 connectDb()
 
-app.use(express.static('public'))
+//body parser to json
 app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-//app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}))
+
+//EXPRESS SESSIONS
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookies:{
+    maxAge: 600000,
+    secure: false
+  }
+}))
+
+
 
 //VIEWS
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-
+app.set("view engine", "ejs")
+app.set("views", "views")
 
 //LAYOUTS
 app.use(expressLayouts)
-app.set('layout', 'layouts/main')
+app.set("layout", "layouts/main")
 
+//STATIC
+app.use(express.static('public'))
 
 //ROUTING
-app.use('/', require('./server/routes/admin_routes.js'))
-app.use('/', require('./server/routes/routes.js'))
-  
+app.use("/", require('./server/routes/admin_routes.js'))
+app.use("/", require('./server/routes/routes.js'))
+
