5dd0b3a2c7c2c031c5da24fa7304f0d1e1278fe5
simplysaad <saadidris70@gmail.com>
2025-07-11 00:39:57 +0100

    make som changes

--- PUBLIC/JS/index.js
+++ PUBLIC/JS/index.js
@@ -1,12 +1,27 @@
+let metaDescription = document
+    .querySelector("meta[name='description']")
+    .getAttribute("content");
+
 const btnShare = document.getElementById("btnShare");
 btnShare.addEventListener("click", async e => {
     if (navigator.share) {
         await navigator.share({
             title: document.title,
-            text: document.description,
+            text: metaDescription,
             url: window.location.href
         });
     } else {
         alert("this device does not allow sharing");
     }
+});
+
+let socials = {
+    whatsapp: `https://wa.me/?text=${document.title}%0A${metaDescription}%0A${window.location.href}`,
+    facebook: `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`,
+    twitter: `https://x.com/intent/tweet?url=${window.location.href}&text=${document.title}%0A${metaDescription}`
+};
+
+let shareIcons = document.querySelectorAll(".share-icon");
+shareIcons.forEach(icon => {
+    icon.href = socials[icon.getAttribute("id")];
 });
