db6bb6c12b6e6baae7b5f87ceb0be1b743c686f6
simplysaad <saadidris70@gmail.com>
2025-07-16 18:46:54 +0100

    refine the gemini prompt

--- Server/Utils/automate.js
+++ Server/Utils/automate.js
@@ -4,11 +4,32 @@ const cron = require("node-cron");
 const generateTopic = require("./generateTopic.js");
 const generatePost = require("./generatePost.js");
 const postTweet = require("./postTwitter.js");
-const {generateSlug} = require("./generateSlug.js");
+const { generateSlug } = require("./generateSlug.js");
 
 async function automate() {
-    let topic = await generateTopic();
-    let post = await generatePost(topic);
+    let post = await generatePost();
+    let query = post.keywords.join("+");
+
+    let unsplashResponse = await fetch(
+        `https://api.unsplash.com/search/photos?query=${query}`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
+            }
+        }
+    );
+
+    if (!unsplashResponse.ok) {
+        return {
+            success: false,
+            message: "error while retrieving unsplashResults"
+        };
+    }
+
+    let unsplashResults = await unsplashResponse.json();
+    let imageUrl = unsplashResults.results[0].urls.raw;
 
     let response = await fetch(`${process.env.BASE_URL}/admin/posts`, {
         method: "POST",
@@ -16,19 +37,21 @@ async function automate() {
             "Content-Type": "application/json",
             Authorization: process.env.BOT_API_KEY
         },
-        body: JSON.stringify(post)
+        body: JSON.stringify({ ...post, imageUrl })
     });
     let { data: newPost } = await response.json();
 
-    let slug = generateSlug(newPost)
+    let slug = generateSlug(newPost);
     let url = "https://biographyhub.onrender.com/article/" + slug;
-
-    const twitterPost = await postTweet(newPost.description, url);
+    //const twitterPost = await postTweet(newPost.description, url);
 
     return {
-        newPost,
-        twitterPost
+        newPost
+        ,imageUrl
+        //,  twitterPost
     };
 }
 
 module.exports = automate;
+
+//results[0].urls.raw
--- Test.js
+++
@@ -1,5 +0,0 @@
-let text = "```json saad is *good* ```";
-
-let cleaned = text.replace(/```json|```|\*/g, "");
-//let cleaned = text.replace(/```json|```|\*/g, "");
-console.log(cleaned.trim());
