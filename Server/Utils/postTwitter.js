7c8197e92ea595c3fa051e0416e4bb1fc3dcfd64
simplysaad <saadidris70@gmail.com>
2025-07-18 07:36:24 +0100

    rwmoved temp files

--- Server/Utils/postTwitter.js
+++ Server/Utils/postTwitter.js
@@ -37,3 +37,58 @@ async function postTweet(text, url, ...paths) {
 }
 
 module.exports = postTweet;
+
+
+
+// To tweet an image from a URL using Node.js and Twitter API v2, you’ll:
+
+// 1. Download the image from the URL (using e.g. `axios` or `request`).
+// 2. Upload the image to Twitter with the v1.1 media endpoint.
+// 3. Post a tweet attaching the received `media_id`.
+
+// Sample code:
+
+// ```js
+// const axios = require('axios');
+// const fs = require('fs');
+// const Twitter = require('twitter-v2');
+// const Twit = require('twit'); // for media upload
+
+// const imageUrl = 'IMAGE_LINK_HERE'; // your image link
+
+// // 1. Download the image
+// axios({ url: imageUrl, responseType: 'stream' }).then(response => {
+//   response.data.pipe(fs.createWriteStream('temp.jpg')).on('finish', () => {
+//     // 2. Upload image using Twit (v1.1)
+//     const T = new Twit({
+//       consumer_key: 'YOUR_CONSUMER_KEY',
+//       consumer_secret: 'YOUR_CONSUMER_SECRET',
+//       access_token: 'YOUR_ACCESS_TOKEN',
+//       access_token_secret: 'YOUR_ACCESS_SECRET'
+//     });
+
+//     const b64content = fs.readFileSync('temp.jpg', { encoding: 'base64' });
+
+//     T.post('media/upload', { media_data: b64content }, (err, data, response) => {
+//       const mediaIdStr = data.media_id_string;
+//       // 3. Post tweet with media using Twitter v2
+//       const client = new Twitter({
+//         consumer_key: 'YOUR_CONSUMER_KEY',
+//         consumer_secret: 'YOUR_CONSUMER_SECRET',
+//         access_token_key: 'YOUR_ACCESS_TOKEN',
+//         access_token_secret: 'YOUR_ACCESS_SECRET'
+//       });
+
+//       client.post('tweets', {
+//         text: 'Check out this photo!',
+//         media: { media_ids: [mediaIdStr] }
+//       }).then(() => {
+//         console.log('Tweet posted!');
+//       });
+//     });
+//   });
+// });
+// ```
+
+// Replace keys and image link with your actual data.  
+// Want help with installing dependencies or getting your API keys? 🚀
